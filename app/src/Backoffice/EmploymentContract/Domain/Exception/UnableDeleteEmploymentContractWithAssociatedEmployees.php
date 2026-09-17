46712a4a9816bfa16f746f17cd3b1f945a6f6fe2
Jorge Romera <romera.gomez.jorge@gmail.com>
2026-09-17 11:15:39 -0300

    Rename namespace for EmploymentContract exception for clarity

--- app/src/Backoffice/EmploymentContract/Domain/Exception/UnableDeleteEmploymentContractWithAssociatedEmployees.php
+++ app/src/Backoffice/EmploymentContract/Domain/Exception/UnableDeleteEmploymentContractWithAssociatedEmployees.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Backoffice\Employee\Domain\Exception;
+namespace App\Backoffice\EmploymentContract\Domain\Exception;
 
 use App\Shared\Domain\DomainError;
 
